7b901a09fe078fd99b2f546d2de8ecfb344815a3
josephx86 <simplex86@gmail.com>
2016-04-15 19:12:36 -0500

    Added contact section

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
     integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
     crossorigin="anonymous">
-<link href="styles.css?d=<?php echo time();?>" rel="stylesheet" type="text/css" />
+<link href="styles.css?d<?php echo time();?>" rel="stylesheet" type="text/css" />
 <link rel="stylesheet"
     href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 </head>
@@ -60,16 +60,16 @@
                     being my favorite.
                 </p>
             </div>
-            <ul id="social-icons">
-                <li><a
+            <div id="about-social">
+                <a
                     href="https://www.linkedin.com/in/joseph-unworried-25531442?trk=nav_responsive_tab_profile"
                     target="_blank" title="LinkedIn"><i
-                        class="fa fa-linkedin-square fa-3x"></i></a></li>
-                <li><a href="https://github.com/josephx86" target="_blank"
-                    title="GitHub"><i class="fa fa-github-square fa-3x"></i></a></li>
-                <li><a href="http://fancyridez.tumblr.com/" target="_blank"
-                    title="Tumblr"><i class="fa fa-tumblr-square fa-3x"></i></a></li>
-            </ul>
+                    class="fa fa-linkedin-square fa-3x"></i></a> <a
+                    href="https://github.com/josephx86" target="_blank"
+                    title="GitHub"><i class="fa fa-github-square fa-3x"></i></a>
+                <a href="http://fancyridez.tumblr.com/" target="_blank"
+                    title="Tumblr"><i class="fa fa-tumblr-square fa-3x"></i></a>
+            </div>
         </div>
         <section id="portfolio-section" class="section-box">
             <p class="section-title">Portfolio</p>
@@ -134,8 +134,55 @@
             </div>
         </section>
         <section id="contact-section" class="section-box">
-            <p>Contact</p>
+            <p class="section-title">Contact</p>
+            <div class="separator"></div>
+            <p class="text-center padded-row">If you like my work, or maybe you
+                have a question to ask, feel free to send me a message below.</p>
+            <form>
+                <div class="row">
+                    <div class="col-xs-1">Name:</div>
+                </div>
+                <div class="row padded-row">
+                    <div class="col-xs-6">
+                        <input type="text" class="form-control" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-1">Email:</div>
+                </div>
+                <div class="row padded-row">
+                    <div class="col-xs-6">
+                        <input type="email" class="form-control" required />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-1">Message:</div>
+                </div>
+                <div class="row padded-row">
+                    <div class="col-xs-11">
+                        <textarea id="message-box" rows="6"
+                            placeholder="Type your message here..." required></textarea>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-1 right-div"></div>
+                    <div class="col-xs-4 right-div">
+                        <button type="submit" class="btn btn-block btn-default">
+                            <span> Send <i class="fa fa-paper-plane"></i></span>
+                        </button>
+                    </div>
+                </div>
+            </form>
         </section>
+        <footer>
+            <p class="text-center">Copyright &copy; 2016 - Joseph
+            
+            
+            <p class="text-center">
+                Free images from <a href="http://www.gratisography.com/"
+                    target="_blank">gratisography.com</a>
+            </p>
+        </footer>
         <script src="https://code.jquery.com/jquery-1.12.3.min.js"
             type="text/javascript"></script>
         <script src="script.js" type="text/javascript"></script>
